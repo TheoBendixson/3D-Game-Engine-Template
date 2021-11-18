e6787b027453eb4ad8630e6863a3883101efd14a
Theodore Bendixson <tedbendixson@me.com>
2021-11-18 15:50:05 -0500

    Rotating one side of a cube

--- code/game_library/code/vario.cpp
+++ code/game_library/code/vario.cpp
@@ -69,9 +69,28 @@ GAME_UPDATE_AND_RENDER(GameUpdateAndRender)
     r32 Near = 1000.0f;
     r32 Far = 1000000.0f;
 
-    vector_float_3 ModelRotation = { 0.0f, 0.0f, 0.0f };
+    local_persist vector_float_3 ModelRotation = { 0.0f, 0.0f, 0.0f };
     vector_float_3 ModelScale = { 400.0f, 400.0f, 400.0f };
     vector_float_3 ModelTranslation = { 0.0f, 0.0f, 1500.0f };
+
+    ModelRotation.X += 0.005f;
+    ModelRotation.Y += 0.009f;
+    ModelRotation.Z += 0.001f;
+
+    matrix RotateX = { 1, 0,                            0,                              0,
+                       0, (r32)(cos(ModelRotation.X)),  -(r32)(sin(ModelRotation.X)),   0,
+                       0, (r32)(sin(ModelRotation.X)),  (r32)(cos(ModelRotation.X)),    0,
+                       0, 0,                            0,                              1 };
+
+    matrix RotateY = { (r32)(cos(ModelRotation.Y)),     0,  (r32)(sin(ModelRotation.Y)),    0,
+                       0,                               1,  0,                              0,                           
+                       -(r32)(sin(ModelRotation.Y)),    0,  (r32)(cos(ModelRotation.Y)),    0,
+                       0,                               0,  0,                              1 };
+
+    matrix RotateZ = { (r32)(cos(ModelRotation.Z)), -(r32)(sin(ModelRotation.Z)),   0,  0,
+                       (r32)(sin(ModelRotation.Z)), (r32)(cos(ModelRotation.Z)),    0,  0,
+                       0,                           0,                              1,  0,
+                       0,                           0,                              0,  1 };
 
     matrix Scale = { ModelScale.X,  0,              0,              0,
                      0,             ModelScale.Y,   0,              0,
@@ -87,7 +106,7 @@ GAME_UPDATE_AND_RENDER(GameUpdateAndRender)
     u32 ViewportHeight = RenderCommands->ViewportHeight;
 
     game_constants *Constants = &RenderCommands->Constants;
-    Constants->Transform = Scale * Translate;
+    Constants->Transform = RotateX * RotateY * RotateZ * Scale * Translate;
     Constants->Projection = { (2 * Near / ViewportWidth), 0,                           0,                           0,
                               0,                          (2 * Near / ViewportHeight), 0,                           0,
                               0,                          0,                           (Far / (Far - Near)),        1,
