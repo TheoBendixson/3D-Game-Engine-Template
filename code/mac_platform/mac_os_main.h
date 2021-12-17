ca7838e171b7def28dd18191dfbba9626a4e6572
Ted Bendixson <tedbendixson@me.com>
2021-12-17 16:52:59 -0700

    Supporting keyboard input on the Mac platform

--- code/mac_platform/mac_os_main.h
+++ code/mac_platform/mac_os_main.h
@@ -1,3 +1,61 @@
 #include "../game_library/code/vario.h"
 
+const unsigned short LeftArrowKeyCode = 0x7B;
+const unsigned short RightArrowKeyCode = 0x7C;
+const unsigned short DownArrowKeyCode = 0x7D;
+const unsigned short UpArrowKeyCode = 0x7E;
+const unsigned short AKeyCode = 0x00;
+const unsigned short SKeyCode = 0x01;
+const unsigned short DKeyCode = 0x02;
+const unsigned short FKeyCode = 0x03;
+const unsigned short RKeyCode = 0x0F;
+const unsigned short LKeyCode = 0x25;
+const unsigned short WKeyCode = 0x0D;
+const unsigned short ReturnKeyCode = 0x24;
+const unsigned short F1KeyCode = 0x7A;
+const unsigned short SpacebarKeyCode = 0x31;
+const unsigned short DeleteKeyCode = 0x33;
+const unsigned short ZKeyCode = 0x06;
+const unsigned short YKeyCode = 0x10;
+
+struct mac_game_controller
+{
+    s32 LeftThumbXUsageID;
+    s32 LeftThumbYUsageID;
+    s32 ButtonAUsageID;
+    s32 ButtonBUsageID;
+    s32 ButtonXUsageID;
+    s32 ButtonYUsageID;
+
+    s32 ButtonLeftShoulder1UsageID;
+    s32 ButtonRightShoulder1UsageID;
+
+    s32 ButtonLeftShoulder2UsageID;
+    s32 ButtonRightShoulder2UsageID;
+    
+    s32 ButtonStartUsageID;
+    s32 ButtonSelectUsageID;
+
+    // Values
+    r32 LeftThumbstickX;
+    r32 LeftThumbstickY;
+    b32 UsesHatSwitch;
+
+    s32 DPadX;
+    s32 DPadY;
+
+    b32 ButtonAState;
+    b32 ButtonBState;
+    b32 ButtonXState;
+    b32 ButtonYState;
+
+    b32 ButtonLeftShoulder1State;
+    b32 ButtonRightShoulder1State;
+
+    b32 ButtonLeftShoulder2State;
+    b32 ButtonRightShoulder2State;
+
+    b32 ButtonStartState;
+    b32 ButtonSelectState;
+};
 
--- code/mac_platform/mac_os_main.mm
+++ code/mac_platform/mac_os_main.mm
@@ -3,18 +3,195 @@
 #include "mac_os_main.h"
 #include "mac_window.m"
 
+global_variable b32 LKeyWasPressed = false;
+global_variable b32 MouseCursorHideAllowed = true;
+global_variable b32 ExternalMouseCursorFlag = false;
+
 @interface GameWindow: NSWindow
+-(void) setMacKeyboardControllerPtr:(mac_game_controller *)MacKeyboardControllerPtr;
 @end
 
 @implementation GameWindow
 {
+    mac_game_controller *_MacKeyboardControllerPtr;
+    game_input *_GameInputPtr;
 }
+
+-(void) setMacKeyboardControllerPtr:(mac_game_controller *)MacKeyboardControllerPtr
+{
+    _MacKeyboardControllerPtr = MacKeyboardControllerPtr;
+}
+
+- (void) setGameInputPtr:(game_input *)GameInputPtr
+{
+    _GameInputPtr = GameInputPtr;
+}
+
+// TODO: (Ted)  Consider looking at this in more detail.
+//              Technically, the mouse cursor doesn't always hide
+//              after five seconds. It is variable depending on when
+//              the user started moving the mouse.
+//
+//              However, just playing with it, this feels fine to me.
+- (void)mouseMoved: (NSEvent *)event
+{
+    [NSCursor unhide];
+
+    if (!ExternalMouseCursorFlag)
+    {
+        MouseCursorHideAllowed = true;
+
+        NSTimeInterval FiveSeconds = 5;
+        [NSTimer scheduledTimerWithTimeInterval: FiveSeconds  
+                                        repeats: false 
+                                          block: ^(NSTimer *timer) {
+            if (MouseCursorHideAllowed && !ExternalMouseCursorFlag)
+            {
+                [NSCursor hide];                                                      
+                MouseCursorHideAllowed = false;
+            }
+        }];
+    }
+
+}
+
+- (void)keyDown:(NSEvent *)theEvent 
+{ 
+    if (theEvent.modifierFlags & NSEventModifierFlagCommand)
+    {
+        return;
+    }
+
+    unsigned short KeyCode = theEvent.keyCode;
+
+    if (KeyCode == LeftArrowKeyCode)
+    {
+        _MacKeyboardControllerPtr->DPadX = -1;
+    }
+    else if (KeyCode == RightArrowKeyCode)
+    {
+        _MacKeyboardControllerPtr->DPadX = 1;
+    }
+    else if (KeyCode == DownArrowKeyCode)
+    {
+        _MacKeyboardControllerPtr->DPadY = -1;
+    }
+    else if (KeyCode == UpArrowKeyCode)
+    {
+        _MacKeyboardControllerPtr->DPadY = 1;
+    }
+    else if (KeyCode == SpacebarKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonAState = 1;
+    }
+    else if (KeyCode == SKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonBState = 1;
+    }
+    else if (KeyCode == FKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonYState = 1;
+    }
+    else if (KeyCode == RKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonSelectState = 1;
+    } 
+    else if (KeyCode == WKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonLeftShoulder1State = 1;
+    } else if (KeyCode == LKeyCode)
+    {
+        LKeyWasPressed = true;
+    } else if (KeyCode == ReturnKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonStartState = 1;
+    } else if (KeyCode == F1KeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonRightShoulder1State = 1;
+    } else if (KeyCode == ZKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonLeftShoulder2State = 1;
+    } else if (KeyCode == YKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonRightShoulder2State = 1;
+    }
+}
+
+- (void)keyUp:(NSEvent *)theEvent
+{
+    unsigned short KeyCode = theEvent.keyCode;
+
+    if (KeyCode == LeftArrowKeyCode || KeyCode == RightArrowKeyCode)
+    {
+        _MacKeyboardControllerPtr->DPadX = 0;
+    } 
+    else if (KeyCode == DownArrowKeyCode || KeyCode == UpArrowKeyCode)
+    {
+        _MacKeyboardControllerPtr->DPadY = 0;
+    }
+    else if (KeyCode == SpacebarKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonAState = 0;
+    }
+    else if (KeyCode == SKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonBState = 0;
+    }
+    else if (KeyCode == DKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonXState = 0;
+    }
+    else if (KeyCode == FKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonYState = 0;
+    }
+    else if (KeyCode == ZKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonLeftShoulder2State = 0;
+    }
+    else if (KeyCode == RKeyCode) 
+    {
+        _MacKeyboardControllerPtr->ButtonSelectState = 0;
+    } 
+    else if (KeyCode == WKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonLeftShoulder1State = 0;
+    }
+    else if (KeyCode == ReturnKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonStartState = 0;
+    } else if (KeyCode == F1KeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonRightShoulder1State = 0;
+    } else if (KeyCode == YKeyCode)
+    {
+        _MacKeyboardControllerPtr->ButtonRightShoulder2State = 0;
+    }
+}
+
 @end
 
 global_variable GameWindow *Window;
 
 int main(int argc, const char * argv[]) 
 {
+    /*
+    mac_state MacState = {};
+    mac_app_path Path = {};
+    MacState.Path = &Path;
+    MacBuildAppFilePath(MacState.Path);
+
+	char SourceGameCodeDLLFullPath[MAC_MAX_FILENAME_SIZE];
+    MacBuildAppPathFileName(MacState.Path, "Contents/Resources/GameCode.dylib",
+                               sizeof(SourceGameCodeDLLFullPath), SourceGameCodeDLLFullPath);
+    mac_game_code Game = MacLoadGameCode(SourceGameCodeDLLFullPath);
+
+    if (!Game.IsValid)
+    {
+        [NSException raise: @"Game Code Not Loaded"
+                     format: @"Game Code Not Loaded"];
+    }*/
+
     MainWindowDelegate *WindowDelegate = [[MainWindowDelegate alloc] init];
 
     NSRect ScreenRect = [[NSScreen mainScreen] frame];
@@ -36,5 +213,8 @@ int main(int argc, const char * argv[])
     [Window setTitle: @"Vario's Temple"];
     [Window makeKeyAndOrderFront: nil];
     [Window setDelegate: WindowDelegate];
+
+
+
     return NSApplicationMain(argc, argv);
 }
