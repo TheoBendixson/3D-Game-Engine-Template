4b58107697576d166b602abb544e360e9f444ef0
Ted Bendixson <tedbendixson@me.com>
2021-12-17 16:09:54 -0700

    Creating the mac build directory when building. Additonal project setup

--- code/mac_platform/build.sh
+++ code/mac_platform/build.sh
@@ -1,6 +1,9 @@
 echo Building Varios Temple
 
-pushd ../../build/mac_os
+MAC_BUILD_PATH="../../build/mac_os"
+
+mkdir -p $MAC_BUILD_PATH
+pushd $MAC_BUILD_PATH
 
 MAC_PLATFORM_LAYER_PATH="../../code/mac_platform"
 GAME_LIBRARY_CODE_PATH="../../code/game_library/code"
@@ -23,7 +26,7 @@ xcrun -sdk macosx metal -mmacosx-version-min=10.14 -gline-tables-only -MO -g -c 
 xcrun -sdk macosx metallib shaders.air -o shaders.metallib
 
 echo Compiling Game Library \(Slow\)
-clang -g -o GameCode.dylib ${COMMON_COMPILER_FLAGS} -dynamiclib ${GAME_LIBRARY_CODE_PATH}/vario.cpp
+clang -g -std=c++11 -o GameCode.dylib ${COMMON_COMPILER_FLAGS} -dynamiclib ${GAME_LIBRARY_CODE_PATH}/vario.cpp
 
 echo Compiling Mac Platform Layer \(Slow\)
 clang -g -lstdc++ ${COMMON_COMPILER_FLAGS} -mmacosx-version-min=10.14 -o Vario "${MAC_PLATFORM_LAYER_PATH}/mac_os_main.mm"
