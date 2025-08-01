997d59cc687d067c886e2928e6381eb1f5cb68dc
LPKNV <46848124+lpknv@users.noreply.github.com>
2025-08-01 13:23:45 +0200

    Update build.bat
    ```
    copy ..\..\code\windows_platform\resources\SDL2.dll SDL2.dll
    ```
    
    This path does not exist and it is why my windows build failed.
    I changed it to...
    
    ```
    copy ..\..\resources\SDL2.dll SDL2.dll
    ```
    and everything works fine now.

--- code/windows_platform/build.bat
+++ code/windows_platform/build.bat
@@ -33,7 +33,7 @@ cl -WX -W3 %ignored_warnings% %optimizer_flags% -DWINDOWS=1 -DGAME=1 /FeVariosTe
            /link /LIBPATH %sdl_library_path%/lib/x64/SDL2.lib^
     %included_libraries%
 
-copy ..\..\code\windows_platform\resources\SDL2.dll SDL2.dll
+copy ..\..\resources\SDL2.dll SDL2.dll
 copy %resources_path%\game_character.png game_character.png
 copy %resources_path%\grid_surface.png grid_surface.png
 copy %resources_path%\cartoon.obj cartoon.obj
